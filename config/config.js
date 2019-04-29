2089e5e6709795a2fa81bd193aa38e1d2b967f79
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-29 20:01:22 +0700

    Edit heroku config

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": "dcq6evf750tdjb",
+        "host": "ec2-54-225-76-136.compute-1.amazonaws.com",
+        "dialect": "postgres"
     }
 }
