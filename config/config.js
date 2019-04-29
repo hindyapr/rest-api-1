1f4742f606f045ec1ce7f6c8653d381705594196
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-29 20:21:31 +0700

    Testing heroku config

--- config/config.js
+++ config/config.js
@@ -14,8 +14,8 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
+        "username": "yfrzzjkwriajsf",
+        "password": "3e69441efc6129274cade5678d0fa790fb54ce2926c309d1c7a2b4a39553809b",
         "database": "dcq6evf750tdjb",
         "host": "ec2-54-225-76-136.compute-1.amazonaws.com",
         "dialect": "postgres"
